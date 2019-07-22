2eee973734d356bd75542e283b6b82e48a303d4b
IvanDev <IvanDeveloper84@ya.ru>
2019-07-22 21:30:18 +0700

    add csrf and get regist mapping

--- src/main/java/my/test/azoft/config/WebSecurityConfig.java
+++ src/main/java/my/test/azoft/config/WebSecurityConfig.java
@@ -2,18 +2,13 @@ package my.test.azoft.config;
 
 import my.test.azoft.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -24,26 +19,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index","/add", "/index/edit", "/index/add", "/registration", "/static/**", "/activate/*").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/index", "/add", "/index/edit", "/index/add", "/registration", "/static/**", "/activate/*").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .rememberMe()
+                    .rememberMe()
                 .and()
-                .logout()
-                .permitAll();
+                    .logout()
+                    .permitAll();
     }
 
-     @Override
-     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         auth.userDetailsService(userService)
-                 .passwordEncoder(passwordEncoder);
-     }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder);
+    }
 }
--- src/main/java/my/test/azoft/controller/RegistrationController.java
+++ src/main/java/my/test/azoft/controller/RegistrationController.java
@@ -3,9 +3,18 @@ package my.test.azoft.controller;
 import my.test.azoft.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/registration")
 public class RegistrationController {
     @Autowired
     private UserService userService;
+
+    @GetMapping
+    public String registration() {
+        return "registration";
+    }
+
 }
