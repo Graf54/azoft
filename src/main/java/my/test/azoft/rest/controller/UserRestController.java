15015215d5826c6b45a8ed7b138964095965bc39
IvanDev <IvanDeveloper84@ya.ru>
2019-07-31 19:20:50 +0700

    make rest for users without admin part

--- src/main/java/my/test/azoft/rest/controller/UserRestController.java
+++ src/main/java/my/test/azoft/rest/controller/UserRestController.java
@@ -1,17 +1,22 @@
 package my.test.azoft.rest.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import my.test.azoft.model.User;
+import my.test.azoft.model.Views;
+import my.test.azoft.rest.exception.AlreadyExistException;
 import my.test.azoft.rest.exception.NotFoundException;
 import my.test.azoft.services.UserService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("api/user")
 public class UserRestController {
 
     private final UserService userService;
@@ -21,36 +26,46 @@ public class UserRestController {
     }
 
     @GetMapping
-    public Page<User> list() {
-        return null;
+    @JsonView(Views.User.class)
+    public Page<User> list(
+            @RequestParam(value = "filter", required = false) String filter,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.ASC) Pageable pageable) {
+
+        //todo temp get list for admin -> delete
+        User admin = userService.findById(1).get();
+
+        return userService.findByUsernameContaining(Optional.ofNullable(filter), pageable, admin);
     }
 
+
     @GetMapping("{id}")
+    @JsonView(Views.User.class)
     public User getOne(@PathVariable int id) {
         Optional<User> optionalUser = userService.findById(id);
-        if (optionalUser.isPresent()) {
-            return optionalUser.get();
-        } else {
-            throw new NotFoundException();
-        }
+        return optionalUser.orElseThrow(NotFoundException::new);
     }
 
     @PostMapping("/create")
-    public User create(@Valid User user) {
+    @JsonView(Views.User.class)
+    public User create(@RequestBody @Valid User user) {
         Optional<User> optionalUser = userService.createUser(user);
-        //todo check present
-        return optionalUser.get();
+        return optionalUser.orElseThrow(AlreadyExistException::new);
     }
 
-    @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable int id, @RequestBody Map<String, String> expenses) {
 
-        return null;
+    @PutMapping("{id}")
+    @JsonView(Views.User.class)
+    public User update(@RequestBody @Valid User user) {
+        return userService.updateUser(user).orElseThrow(NotFoundException::new);
     }
 
 
     @DeleteMapping("{id}")
     public void delete(@PathVariable int id) {
-        userService.deleteById(id);
+        if (userService.findById(id).isPresent()) {
+            userService.deleteById(id);
+        } else {
+            throw new NotFoundException();
+        }
     }
 }
